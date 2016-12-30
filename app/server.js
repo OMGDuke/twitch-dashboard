63f5b2d42a2cddad9ca468155b48bd7ad46e8d7e
Alex Robertson <lex1989@gmail.com>
2016-12-30 21:19:09 +0000

    File structure and server.js setup

---
+++ app/server.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
+var mongoose = require('mongoose');
+var passport = require('passport');
+
+var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+
+var configDB = require('./app/config/database.js')
+
+mongoose.connect(configDB.url);
+
+app.use(express.static(__dirname + '/public'));
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(bodyParser());
+app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.json());
+// app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+app.use(session({secret: process.env.SESSION_SECRET}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(passport.session());
+
+require('./app/routes.js')(app, passport);
+
+app.listen(port);
+console.log('Server launched on port: ' + port);
